50ecc91c7d43eb1ef22e784e5543345327f902d0
Yukun Shang <shangyukun.ryan@bytedance.com>
2024-02-26 13:29:42 +0800

    add account

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 mod trader;
-use trader::{Strategy, StrategyEngine};
+use trader::{Strategy, StrategyEngine, Account};
 
 fn main() {
     env_logger::init();
-
+    let account = Account::new(100.0, 100.0);
     let strategy = Strategy::new(5.0, 5.0);
     let engine = StrategyEngine::new(strategy);
     engine.run();
--- src/trader.rs
+++ src/trader.rs
@@ -187,8 +187,6 @@ impl StrategyEngine {
     }
 }
 
-
-
 #[derive(Debug)]
 struct Order{
     id: String,
@@ -211,11 +209,26 @@ struct OrderBook {
     price_list:Vec<i32>
 }
 
-struct Portfolio {
+pub struct Account{
     balance: f32,
+    position: f32,
     gain: f32,
     alpha: f32,
     beta:f32,
     sharp:f32,
     max_withdraw: f32,
+}
+
+impl Account {
+    pub fn new(balance: f32, position: f32) -> Account {
+        Account{
+            balance,
+            position,
+            gain: 0.0,
+            alpha: 0.0,
+            beta: 0.0,
+            sharp: 0.0,
+            max_withdraw: 0.0,
+        }
+    }
 }
